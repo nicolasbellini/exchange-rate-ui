7823f0afde170d854ec4e450eedc78a433b78738
nicolasbellini <nicolasgbellini@gmail.com>
2022-01-18 19:07:59 -0300

    added ui for rates

---
+++ src/rates/AllRates.js
@@ -0,0 +1,55 @@
+import { Card, Grid, CardContent, Typography } from '@mui/material';
+import axios from 'axios';
+import React,{useEffect, useState} from 'react';
+
+function AllRates(){
+    const [rates,setRates] = useState([])
+    const [showRates,setShow] = useState(false);
+    console.log()
+
+    useEffect(()=>{
+        getPost()
+      },[])
+
+
+    const getPost = () =>{
+        axios.get("http://localhost:1337/api/exchangeRate/getAllRates").then(response=>{
+            let responseRates = JSON.parse(JSON.stringify(response.data.result))
+            setRates(responseRates)
+            setShow(true)
+        }).catch((error)=> console.log("Error: ", error));
+    }
+
+    return(
+        <>
+        <Typography variant='h2' component="div">
+          All saved rates
+        </Typography>
+        <Grid container spacing={1} justify="center">
+            {
+                showRates && rates.map(rate => <Grid item>     <Card sx={{ minWidth: 275 }}>
+                    <CardContent sx={{ bgcolor: 'text.disabled' }}>
+                      <Typography variant="h5" component="div">
+                      </Typography>
+                      <Typography sx={{ fontSize: 14 }} color="green" gutterBottom>
+                        {rate.source}
+                      </Typography>
+                      <Typography sx={{ fontSize: 12 }} color="text.secondary" gutterBottom>
+                        to
+                      </Typography>
+                      <Typography sx={{ fontSize: 14 }} color="blue" gutterBottom>
+                        {rate.target}
+                      </Typography>
+                      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        {rate.rate}
+                      </Typography>
+                    </CardContent>
+                  </Card> </Grid>)
+            }
+        </Grid>
+        </>
+    )
+
+}
+
+export default AllRates
---
+++ src/rates/NewRate.js
@@ -0,0 +1,60 @@
+import { Alert, Box, Button, TextField, Typography } from '@mui/material';
+import axios from 'axios';
+import React,{useState} from 'react';
+
+function NewRate(){
+    const [src,setSource] = useState([''])
+    const [trg,setTarget] = useState([''])
+    const [showAlert, setShowAlert] = useState(null);
+    const [showAlert404, setShowAlert404] = useState(null);
+
+
+    const handleChangeSource = e => {
+        setSource(e.target.value);
+      };
+
+    const handleChangeTarget = e => {
+        setTarget(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios.post("http://localhost:1337/api/exchangeRate/createOrUpdate", {src, trg}).then(r => {
+            setSource("")
+            setTarget("")
+            setShowAlert(true)
+            window.location.reload(false);
+        }).catch(reason => {
+            setSource("")
+            setTarget("")
+            setShowAlert404(true)
+        })
+    } 
+
+    return(
+        <>
+        <Typography variant='h2'>
+            Save a new rate
+        </Typography>
+        <Box component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+            >
+            <TextField label="SOURCE" focused  onChange={handleChangeSource}/>
+            <TextField label="TARGET" focused  onChange={handleChangeTarget}/>
+            <Button variant="contained" color="primary" onClick={handleSubmit}>
+                Submit
+            </Button>
+            {showAlert && <Alert severity="success" onClose={() => {setShowAlert(false)}}>Rate succesfully added!</Alert>}
+            {showAlert404 && <Alert severity="error" onClose={() => {setShowAlert404(false)}}>That rate does not exist!</Alert>}
+        </Box>
+        </>
+    )
+
+
+}
+
+export default NewRate
