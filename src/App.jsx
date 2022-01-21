639ac83494b46cdc83039de8404f774f1e8336d0
nicolasbellini <nicolasgbellini@gmail.com>
2022-01-21 16:31:56 -0300

    added mui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Container, Grid } from '@mui/material';
 import AllRates from "./components/AllRates";
 import LeftBar from './components/LeftBar';
+import ResponsiveAppBar from './components/NavBar';
 import NewRate from './components/NewRate';
 
 const App = () => {
   
     return(
     <div>
-      <LeftBar></LeftBar>
+      <ResponsiveAppBar></ResponsiveAppBar>
         <Grid Container>
           <NewRate/>
           <AllRates/>
